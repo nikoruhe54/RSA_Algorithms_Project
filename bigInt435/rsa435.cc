56165ae13c45e4c1b039732e1a04d1073e06c7ba
Nikolai Ruhe <nikoruhe54@gmail.com>
2018-01-26 13:28:46 -0500

    first commit to upload original source code

---
+++ bigInt435/rsa435.cc
@@ -0,0 +1,46 @@
+// You need to complete this program for a part of your first project.
+
+// Standard libraries
+#include <string>
+#include <iostream>
+#include <stdlib.h> 
+
+
+// `BigIntegerLibrary.hh' includes all of the library headers.
+#include "BigIntegerLibrary.hh"
+
+
+int main() {
+	/* The library throws `const char *' error messages when things go
+	 * wrong.  It's a good idea to catch them using a `try' block like this
+	 * one.  Your C++ compiler might need a command-line option to compile
+	 * code that uses exceptions. */
+	try {
+		      
+      std::cout << "a couple of test cases for 3460:435/535 Algorithms!!!\n";
+      BigInteger big1 = BigInteger(1);
+	  std::cout << big1;
+      for (int i=0;i<400;i++) {
+         big1 = big1*10 +rand();
+      }
+      std::cout << "my big1 !!!\n";
+      std::cout << big1;
+      BigInteger big2 = BigInteger(1);
+      for (int i=0;i<400;i++) {
+         big2 = big2*10 +rand();
+      }
+      std::cout << "my big2 !!!\n";
+      std::cout << big2;
+      std::cout << "my big3 = big1*big2 !!!\n";
+      BigInteger big3 = big1*big2;
+      std::cout <<big3;
+      std::cout << "my big3/big2 !!!\n";
+      std::cout <<big3/big2;
+      
+	} catch(char const* err) {
+		std::cout << "The library threw an exception:\n"
+			<< err << std::endl;
+	}
+
+	return 0;
+}
