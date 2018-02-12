7a356fec5acea19060f9b0d6d18396eab49d40b8
Nikolai Ruhe <nikoruhe54@gmail.com>
2018-02-12 13:14:42 -0500

    debug

--- messageDigest/main.cpp
+++ messageDigest/main.cpp
@@ -40,17 +40,21 @@ int main(int argc, char *argv[])
 	//convert the signature to a bigUnsigned
     cout << "sha256('"<< str << "'):" << output01 << endl;
 	BigUnsigned sig01 = stringToBigUnsigned(output01);
-
+	cout << "converted string to bigUnsigned \n";
 	//import the private key and n
 	std::ifstream privateKeyFile;
 	privateKeyFile.open("d_n.txt");
+	cout << "opened the d_n.txt file \n";
 	string d_str, n_str;
 	std::getline(privateKeyFile, d_str);
 	std::getline(privateKeyFile, n_str);
+	cout << "got the variables \n";
 	BigUnsigned privateKey = stringToBigUnsigned(d_str);
+	cout << "got the privateKey \n";
 	BigUnsigned n = stringToBigUnsigned(n_str);
-
+	cout << "got the n \n";
 	//apply signature to output01
 	BigUnsigned signature01 = modexp(sig01, privateKey, n);
+	cout << "made it to modexp \n";
     return 0;
 }
