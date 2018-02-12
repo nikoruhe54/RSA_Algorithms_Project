fe4762c48c621b00c46369a6770bc61541932424
Nikolai Ruhe <nikoruhe54@gmail.com>
2018-02-12 12:14:50 -0500

    added an input file

--- messageDigest/main.cpp
+++ messageDigest/main.cpp
@@ -6,6 +6,7 @@ sha256('grape'): 0f78fcc486f5315418fbf095e71c0675ee07d318e5ac4d150050cd8e5796649
 **/
 
 #include <iostream>
+#include <fstream>
 #include "sha256.h"
 
 using std::string;
@@ -14,6 +15,25 @@ using std::endl;
  
 int main(int argc, char *argv[])
 {
+	std::string filename = "test.txt"; //test.jpg test.png etc.
+	std::ifstream myfile(filename.c_str(), std::ios::binary);
+	std::streampos begin, end;
+	begin = myfile.tellg();
+	myfile.seekg(0, std::ios::end);
+	end = myfile.tellg();
+	std::streampos size = end - begin; //size of the file in bytes   
+	myfile.seekg(0, std::ios::beg);
+
+	char * memblock = new char[size];
+	myfile.read(memblock, size); //read the entire file
+	memblock[size] = '\0'; //add a terminator
+	myfile.close();
+
+	//check what's in the block
+	string str(memblock);
+	std::cout << str;
+	std::cout << "\nthe content ";
+
     string input = "grapeJuice    sha256('grapeJuice'):0f78fcc486f5315418fbf095e71c0675ee07d318e5ac4d150050cd8e57966496";
     string output1 = sha256(input);
  
